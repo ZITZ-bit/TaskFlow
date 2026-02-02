55b3f838c5fe1c6f107ba17120bc1148f7182258
ZITZ <zitzcode@gmail.com>
2026-02-02 19:56:37 -0400

    Avances: Seed (En Proceso)

--- apps/backend/db/seed/seed.ts
+++ apps/backend/db/seed/seed.ts
@@ -1,45 +1,47 @@
+import "dotenv/config"; // 🔑 cargar .env
 import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  console.log("🌱 Iniciando seed...");
 
   // ---- ROLES ----
-
   await prisma.roles.createMany({
     data: [
       { nombre_rol: "ADMIN", descripcion: "Administrador del sistema" },
       { nombre_rol: "USER", descripcion: "Usuario normal" },
     ],
-
     skipDuplicates: true, // evita errores si ya existen
-
   });
 
   // ---- USUARIO ADMIN ----
-
   const hashedPassword = await bcrypt.hash("zitzkira", 10);
 
-  const admin = await prisma.usuarios.create({
-
-    data: {
-      nombres: "admin",
-      apellidos: "admin",
+  await prisma.usuarios.upsert({
+    where: { usuario: "ZITZ" },
+    update: {}, // no hace nada si ya existe
+    create: {
+      nombres: "Andrew",
+      apellidos: "Carrero",
       cedula: "00000001",
       usuario: "ZITZ",
       clave: hashedPassword,
       usuario_rol: {
         create: [
-          { rol_id: 1 },
+          { rol_id: 1 }, // ADMIN
         ],
       },
     },
-
   });
-  
+
+  console.log("✅ Seed completada!");
 }
 
 main()
-  .catch((e) => console.error(e))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
   .finally(() => prisma.$disconnect());
--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
-import { join } from "path";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "db/migrations",
-    seed: join(__dirname, "db/seed.ts"),
+    seed: 'tsx db/seed/seed.ts',
   },
   datasource: {
     url: process.env["DATABASE_URL"],
